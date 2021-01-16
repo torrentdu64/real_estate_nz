eb66eaad674263e77286ec6cd05dd20fbfbc18db
torrent benoit <torrentdu64@gmail.com>
2021-01-17 10:55:58 +1300

    rails g controller houses new create index show edit update destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'houses/new'
+  get 'houses/create'
+  get 'houses/index'
+  get 'houses/show'
+  get 'houses/edit'
+  get 'houses/update'
+  get 'houses/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
