21c5380acafb5b8d7fcf59cd5ecf7f6e344ef4af
torrent benoit <torrentdu64@gmail.com>
2021-02-08 18:17:12 +1300

    rails db:migrate ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_214858) do
+ActiveRecord::Schema.define(version: 2021_02_08_042915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2021_01_16_214858) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "price"
     t.index ["user_id"], name: "index_houses_on_user_id"
   end
 
